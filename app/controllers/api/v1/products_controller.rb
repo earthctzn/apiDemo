c8d724a277f2d1153624b32e7e28aab1396fdda6
earthctzn <earthctzn1@gmail.com>
2020-08-02 19:42:10 -0400

    Adds prod_params, and create actions to products controller.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,11 +1,35 @@
 class Api::V1::ProductsController < ApplicationController
   def index
     products = Product.all
-    render json: products status: 200
+    render json: products, status: 200
+  end
+
+  def create
+    product = Product.new(
+      name: prod_params[:name],
+      brand: prod_params[:brand],
+      price: prod_params[:price],
+      description: prod_params[:description]
+    )
+    if product.save
+      render json: product, status: 200
+    else
+      render json: {error: "Error creating review."}
+    end
   end
 
   def show
     product = Product.find_by(id: params[:id])
-    render json: product status: 200
+    render json: product, status: 200
   end
+
+  private
+    def prod_params
+      params.required(:product).permit(
+        :name, 
+        :brand, 
+        :price, 
+        :description
+      )
+    end
 end
