a64de91b76fc6339d152bbd8fe14b123e0166abd
earthctzn <earthctzn1@gmail.com>
2020-08-02 21:23:18 -0400

    Adds actions to products_controller with corresponding routes in routes.rb.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::ProductsController < ApplicationController
+
+  # index renders all items in the products table
   def index
     products = Product.all
     render json: products, status: 200
   end
 
+  # As the name implies this action lets us create a new product.
+  # If the product saves correctly, we render the json data for the product.
+  # If the product does not save correctly we render an error object. 
   def create
     product = Product.new(
       name: prod_params[:name],
@@ -18,18 +23,26 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  # This method looks up the product by the id, if it is found we render the json object 
+  # Otherwise we render an error object.
   def show
     product = Product.find_by(id: params[:id])
-    render json: product, status: 200
+    if product
+      render json: product, status: 200
+    else
+      render json: {error: "Product Not Found."}
+    end
   end
 
+  # This private method is only available to this controller.
+  # It uses the the built-in methods .require and .permit provided by ActionController
   private
     def prod_params
-      params.required(:product).permit(
+      params.require(:product).permit([
         :name, 
         :brand, 
         :price, 
         :description
-      )
+      ])
     end
 end
